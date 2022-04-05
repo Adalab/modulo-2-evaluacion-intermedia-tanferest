2cb151eb205163abd22314804366522c5cf9f13a
tanferest <tanferest@gmail.com>
2022-04-05 21:27:33 +0200

    extract painter function

--- js/main.js
+++ js/main.js
@@ -1,30 +1,46 @@
-"use strict";
+'use strict';
 
 // 1. Crear constantes y variables
-const diceSelect = document.querySelector(".js-select");
-const btn = document.querySelector(".js-btn");
-const message = document.querySelector(".js-message");
-const balance = document.querySelector('.js-balance');
-const bet = document.querySelector('.js-bet');
+const diceSelect = document.querySelector('.js-select');
+const btn = document.querySelector('.js-btn');
+const messageBox = document.querySelector('.js-message');
+const balanceContainer = document.querySelector('.js-balance');
+const betInput = document.querySelector('.js-bet');
+let selectedDice = '';
+let computerNum = '';
+let betAmount = '';
+let balanceAmount = 50;
 
 // 2. Crear funciones
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function checkNumber() {
-  const selectedDice = parseInt(diceSelect.value);
-  const computerNum = getRandomNumber(6);
-  const betAmount = parseInt(bet.value);
-  let balanceAmount = 50;
+// Función pintar resultado HTML
+function paintHTML() {
   if (selectedDice === computerNum) {
-    message.innerHTML = `<p>¡Has ganado el doble de lo apostado! :)</p><p>Jugada del ordenador: ${computerNum}</p>`;
-    balanceAmount += betAmount;
+    messageBox.innerHTML = `<p>¡Has ganado el doble de lo apostado! :)</p><p>Jugada del ordenador: ${computerNum}</p>`;
   } else {
-    message.innerHTML = `<p>Has perdido lo apostado :(</p><p>Jugada del ordenador: ${computerNum}</p>`;
-    balanceAmount -= betAmount;
+    messageBox.innerHTML = `<p>Has perdido lo apostado :(</p><p>Jugada del ordenador: ${computerNum}</p>`;
   }
-  balance.innerHTML = `Saldo: ${balanceAmount}`;
+   balanceContainer.innerHTML = `Saldo: ${balanceAmount}`;
 }
 
-btn.addEventListener("click", checkNumber);
+// Comprobar si el número escogido es igual al del ordenador
+function checkNumber() {
+  selectedDice = parseInt(diceSelect.value);
+  computerNum = getRandomNumber(6);
+  betAmount = parseInt(betInput.value);
+  if (selectedDice === computerNum) {
+    balanceAmount += betAmount;
+  } else {
+    balanceAmount -= betAmount;
+  }
+}
+
+function handleClick() {
+  checkNumber();
+  paintHTML();
+}
+
+btn.addEventListener('click', handleClick);
